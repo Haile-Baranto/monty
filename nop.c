8ea3321328c07931d7cbf11902e28c9e1b5731ce
Haile-Baranto <hailebaranto@gmail.com>
2023-03-24 00:00:56 +0300

    Add nop.c file

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  * nop- The opcode nop doesn’t do anything.
+  * @head: pointer to the first element of the stack
+  * @counter: line_number
+  * Return: Nothing
+  * Description: usage: nop.
+ */
+void nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
